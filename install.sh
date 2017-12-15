2cc0374df116f43776ffbb80b0bf554403e237b7
AlEmerich <alan@projet-lucine.org>
2017-12-15 10:55:02 +0100

    tmux config file

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Spacemacs
+
+git clone git@github.com:AlEmerich/spacemacs.git spacemacs/.emacs.d
+ln -s $PWD/spacemacs/.spacemacs ~/.spacemacs
+ln -s $PWD/spacemacs/.emacs.d ~/.emacs.d
+
+# Tmux
+
+alias tmux='tmux -f ~/custom_config/tmux/.tmux.conf'
+
--- spacemacs/install.sh
+++
@@ -1,4 +0,0 @@
-git clone git@github.com:AlEmerich/spacemacs.git .emacs.d
-ln -s $PWD/.spacemacs ~/.spacemacs
-ln -s $PWD/.emacs.d ~/.emacs.d
-
